b06fa436e0f1160b2591916140fb7f38bf92460c
Vinícius de Sousa Carvalho <sousav387@gmail.com>
2025-12-27 16:24:28 -0300

    feat: first commit

---
+++ module_01/HelloGoodbye.java
@@ -0,0 +1,16 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class HelloGoodbye {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            StdOut.println("Hello " + args[0] + " and " + args[1] + ".");
+            StdOut.println("Goodbye " + args[1] + " and " + args[0] + ".");
+        }
+    }
+}
